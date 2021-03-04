d8fe8fe5111c3014cae725273cc26beb01a99e6d
Chikara <chtakahashi10@gmail.com>
2021-03-04 17:10:15 -0500

    feat: adds all parsing logic. TODO: unicode verification and error handling

--- normalizer.js
+++ normalizer.js
@@ -2,24 +2,58 @@
 
 const readline = require('readline')
 
-const normalizeCsvLine = (line) => {
-  // TO DO:
-  // For all rows: Invalid characters can be replaced with the Unicode Replacement Character. If that replacement makes data invalid (for example, because it turns a date field into something unparseable), print a warning to stderr and drop the row from your output.
-  // Timestamp to RFC3339 format + convert to US/Eastern time
-  // ZIP codes should be 5 digits. pad missing leading zeros
-  // Address - Unicode validation only
-  // Foo duration and bar durations - convert these to total number of seconds.
-  // totalDuration = fooduration + barduration
-  // no transformation
-  return line
+// split the incoming csv string with care for commas that appear inside quotes
+const split = (line) => {
+  return line.match(/("[^"]*")|[^,]+/g)
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+const convertDurationToSeconds = (time) => {
+  let temp = time.split(':')
+  return parseInt(temp[0]) * 3600
+    + parseInt(temp[1]) * 60
+    + parseFloat(temp[2])
+}
 
-rl.on('line', (line) => {
-  const normalizedLine = normalizeCsvLine(line)
-  console.log(normalizedLine)
-});
+const normalizeCsvLine = (line) => {
+  const row = split(line)
+  // perform no transformations on the header
+  if (row[0] === 'Timestamp') {
+    return line
+  }
+
+  // INDEX 0: address DateTime
+  const date = new Date(row[0])
+  // date is in Pacific Time. convert to Eastern (add 3 hours)
+  date.setHours(date.getHours() + 3)
+  row[0] = date.toISOString()
+  // INDEX 1: no transformations other than Unicode verification
+
+  // INDEX 2: ensure each zip code is 5 digits
+  row[2] = `00000${row[2]}`.slice(-5)
+
+  // INDEX 3: FullName - capitalize
+  row[3] = row[3].toUpperCase()
+
+  // INDEX 4: FooDuration - convert to seconds
+  row[4] = convertDurationToSeconds(row[4])
+  // INDEX 5: BarDuration - convert to seconds
+  row[5] = convertDurationToSeconds(row[5])
+  // INDEX 6: replace contents with foo + bar
+  row[6] = row[4] + row[5]
+  // INDEX 7: no transformations other than Unicode verification
+  return row.join(',')
+}
+
+const main = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.on('line', (line) => {
+    const normalizedLine = normalizeCsvLine(line)
+    console.log(normalizedLine)
+  });
+}
+
+main();
